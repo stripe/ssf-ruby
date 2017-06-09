60c8f4b00361d1e89f58e2e28f7b4f7b9100a081
Aditya Mukerjee <dev@chimeracoder.net>
2017-06-09 16:58:27 -0400

    Add client

---
+++ lib/ssf/client.rb
@@ -0,0 +1,27 @@
+require 'socket'
+
+module SSF
+  class Client
+
+    DEFAULT_HOST = '127.0.0.1'
+    DEFAULT_PORT = 8200
+
+    attr_reader :host
+    attr_reader :port
+
+    def initialize(host = DEFAULT_HOST, port = DEFAULT_PORT, opts = {}, max_buffer_size=50)
+      self.host, self.port = host, port
+      @socket = connect_to_socket(host, port)
+    end
+
+    def connect_to_socket(host, port)
+      socket = UDPSocket.new
+      socket.connect(host, port)
+      socket
+    end
+
+    def send_to_socket(message)
+      @socket.send(message) 
+    end
+  end
+end
--- test/ssf/sample_test.rb
+++ test/ssf/sample_test.rb
@@ -6,10 +6,22 @@ module SSFTest
 
     def test_create_ssf
       print("asdasdfasd")
-      s = Ssf::SSFSample.new({
+      s = Ssf::SSFSpan.new({
         id: 123456,
       })
       assert(s.id == 123456, "id doesn't match")
+    end
+
+    def test_encode_ssf
+      s = Ssf::SSFSpan.new({
+        id: 123456,
+      })
+
+      message = Ssf::SSFSpan.encode(s)
+
+      s2 = Ssf::SSFSpan.decode(message)
+
+      assert(s.id == s2.id)
     end
   end
 end
